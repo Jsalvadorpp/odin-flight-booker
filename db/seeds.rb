5ab8ec33a5212104bd7eb3384519e10f4006a15e
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-27 15:53:53 -0400

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airport_list = [
+    'VEN','USA','CHE','SWE','SUR','ESP','RUS','MEX','JPN','ITA','ECU''COL'
+]
+
+airport_list.each do |code|
+    Airport.create(code: code)
+end
+
+airports = Airport.all
+
+airports.each do |from_airport|
+
+    airports.each do |to_airport|
+
+        date = DateTime.now + rand(0..8).hour
+        duration = Time.now + rand(1..6).hour
+
+        new_flight = {
+            from_airport: from_airport,
+            to_airport: to_airport,
+            flight_date: date,
+            flight_duration: duration
+        }
+        Flight.create(new_flight) if from_airport != to_airport
+    end
+end
+
+
