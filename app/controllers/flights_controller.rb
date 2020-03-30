6afa3993077e2eeadbe97d787bfeccb44ae073e8
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-30 17:39:02 -0400

    select airports form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,6 @@ class FlightsController < ApplicationController\
 
     def index
         @flights = Flight.all
+        @airports = Airport.all.map { |airport| [airport.code, airport.id] }
     end
 end
